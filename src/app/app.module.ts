9e4b5290861d8c5e1efb4c429831be35ae5f36fd
Ganna Vagner <gvagntc@training.local>
2017-09-26 11:24:58 +0300

    fff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { FooterComponent } from './footer/footer.component';
     NavBarComponent,
     SpecificEventComponent,
     FooterComponent
-  ],
+  ], 
   imports: [
     BrowserModule
   ],
