0d81053058f3636ef14c59ce67869c25eef13482
Ganna Vagner <gvagntc@training.local>
2017-09-27 17:02:38 +0300

    Added agenda and changed nav

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  background={link:"/assets/images/imgFooter.jpg"}
+  background={link:"/assets/images/imgFooter.jpeg"}
   constructor() { }
 
   ngOnInit() {
